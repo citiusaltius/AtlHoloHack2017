4f3c503b3e5f4bb03fd59b778d19b2044ddc9209
citiusaltius <me@aiwong.com>
2017-06-09 21:27:26 -0400

    initial commit

---
+++ HoloHack Building Management System/Assets/BuildingManager.cs
@@ -0,0 +1,45 @@
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using HoloToolkit.Unity.SpatialMapping;
+
+public class BuildingManager : Singleton<BuildingManager>, ISpeechHandler
+{
+
+    public GameObject spatialMapping;
+    public GameObject IOTCollection;
+   
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData)
+    {
+        var voiceCommand = eventData.RecognizedText.ToLower();
+
+        switch (voiceCommand)
+        {
+            case "scan complete":
+                {
+                    SpatialMappingManager.Instance.DrawVisualMeshes = false;
+                    IOTCollection.SetActive(true);
+                    IOTCollection.transform.parent = spatialMapping.transform;
+
+                }
+                break;
+        }
+    }
+}
---
+++ HoloHack Building Management System/Assets/DispayPowerBtn.cs
@@ -0,0 +1,32 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DispayPowerBtn : MonoBehaviour, IInputClickHandler {
+
+    public GameObject display;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+
+        if (!display.activeSelf)
+        {
+            display.SetActive(true);
+        } else
+        {
+            display.SetActive(false);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
